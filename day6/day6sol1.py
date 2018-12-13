58d8b603dc68cb86e278162b48a316642fe8706a
Michael Nilson <Michael.Nilson@Yardi.com>
2018-12-13 13:01:09 -0600

    Day 6 part 1 works for practice file. Runtime error on workfile

--- day6/day6sol1.py
+++ day6/day6sol1.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from string import ascii_lowercase
 from string import ascii_uppercase
 
@@ -15,7 +16,7 @@ def grid_print():
     print('*' * max_x)
     for x in range(0, max_x + 1):
         row = ''
-        for y in range(0, max_y + 1):
+        for y in range(0, max_y):
             if (x, y) in grid:
                 row += grid[(x, y)]
             else:
@@ -30,7 +31,7 @@ def print_map(id, map):
     print('*' * max_x)
     for x in range(0, max_x + 1):
         row = ''
-        for y in range(0, max_y + 1):
+        for y in range(0, max_y):
             if (x, y) in map:
                 num = map[(x, y)]
                 if num > 34:
@@ -74,13 +75,12 @@ def grow_gen_coord(coord, generations_grid, generation):
 
 
 def grow_gen(generations_grid, generation):
-    #doesn't appear to be hitting all keys for a given generation
-    for coord in generations_grid.keys():
+    size = len(generations_grid)
+    coordinates = list(generations_grid.keys())
+    for coord in coordinates:
         if generations_grid[coord] != generation:
             continue
-        print(generation, coord)
 
-        size = len(generations_grid)
         grow_coord = (coord[0] - 1, coord[1])
         generations_grid = grow_gen_coord(grow_coord, generations_grid, generation + 1)
 
@@ -93,11 +93,11 @@ def grow_gen(generations_grid, generation):
         grow_coord = (coord[0], coord[1] - 1)
         generations_grid = grow_gen_coord(grow_coord, generations_grid, generation + 1)
 
-        if (len(generations_grid) == size):
-            #did not grow / done
-            return generations_grid
-        else:
-            return grow_gen(generations_grid, generation+1)
+    if (len(generations_grid) == size):
+        #did not grow / done
+        return generations_grid
+    else:
+        return grow_gen(generations_grid, generation+1)
 
 def grow(seed_growth):
     growth_maps = {}
@@ -106,9 +106,61 @@ def grow(seed_growth):
         gen_zero[coord] = 0
         growth_map = grow_gen(gen_zero, 0)
         growth_maps[id] = growth_map
-        print_map(id, growth_map)
+        # print_map(id, growth_map)
 
-with open('practice.txt', 'r') as file:
+    for x in range (0, max_x+1):
+        for y in range(0, max_y):
+            best_id = ''
+            best_value = 999999
+            for id, growth_map in growth_maps.items():
+                if growth_map[(x, y)] < best_value:
+                    best_value = growth_map[(x, y)]
+                    best_id = id
+                elif growth_map[(x, y)] == best_value:
+                    best_id = '.'
+            if best_id == '.':
+                grid[(x,y)] = best_id
+            elif best_value > 0:
+                grid[(x,y)] = best_id.lower()
+
+    for x in range (0, max_x + 1):
+        if grid[(x, 0)] == '.':
+            continue
+        else:
+            infinite.add(grid[(x,0)].upper())
+
+    for y in range (0, max_y):
+        if grid[(0, y)] == '.':
+            continue
+        else:
+            infinite.add(grid[(0,y)].upper())
+
+    for x in range (0, max_x + 1):
+        if grid[(x, max_y-1)] == '.':
+            continue
+        else:
+            infinite.add(grid[(x,max_y-1)].upper())
+
+    for y in range (0, max_y):
+        if grid[(max_x, y)] == '.':
+            continue
+        else:
+            infinite.add(grid[(max_x,y)].upper())
+
+def calc_max():
+    max_counts = {}
+    for x in range (0, max_x + 1):
+        for y in range(0, max_y):
+            val = grid[(x, y)]
+            if val == '.':
+                continue
+            if val in max_counts:
+                max_counts[val] += 1
+            else:
+                max_counts[val] = 1
+    return max_counts
+
+with open('workfile.txt', 'r') as file:
     for num, line in enumerate(file):
         coords = line.split(", ")
         x = int(coords[0])
@@ -120,9 +172,16 @@ with open('practice.txt', 'r') as file:
 
         coordinates[ascii_uppercase[num]] = (y, x)
         grid[(y, x)] = ascii_uppercase[num]
-        break
 
 
 grid_print()
 grow(grid)
 grid_print()
+print(infinite)
+max_counts = calc_max()
+print(max_counts)
+for id, val in max_counts.items():
+    if id.upper() in infinite:
+        continue
+    print(f'{id}={val}')
+
